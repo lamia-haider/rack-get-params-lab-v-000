8757ca2c45a7974f03beb7d34e4cbe2b4f0f09c4
Lamia Haider <lamia.haider@gmail.com>
2020-10-06 07:39:38 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,7 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
       resp.write "#{cart}"
-        else resp.write "Your cart is empty"
+      else resp.write "Your cart is empty"
     else
       resp.write "Path Not Found"
     end
